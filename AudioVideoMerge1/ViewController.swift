ea3c2a346b7567c72849bd57a44dc7235a997c8b
mmohsin991 <muhammad.mohsin.991@gmail.com>
2015-02-13 20:28:31 +0500

    Initial Commit

---
+++ AudioVideoMerge1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioVideoMerge1
+//
+//  Created by Mohsin on 13/02/2015.
+//  Copyright (c) 2015 PanaCloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
